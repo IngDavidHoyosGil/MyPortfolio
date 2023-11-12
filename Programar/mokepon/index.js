7b4ad75bf24ef8c45a600e17c82069a601eb1537
IngDavidHoyosGil <davidhoyosgil2006@hotmail.com>
2023-11-12 12:29:45 -0500

    7-nodejs

--- Programar/mokepon/index.js
+++ Programar/mokepon/index.js
@@ -92,6 +92,15 @@ app.post("/mokepon/:jugadorId/ataques", (req, res) => {
     res.end()
 })
 
+app.get("/mokepon/:jugadorId/ataques", (req, res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const jugador = jugadores.find((jugador) => jugadorId === jugador.id)
+
+    res.send({
+        ataques: jugador.ataques || []
+    })
+})
+
 app.listen(8080, () => {
     console.log("Servidor funcionando")
 })
